b01a1bbccbb8873f991451a33d974112b22e18d1
Ygor <ygor.brilhante.dev@gmail.com>
2022-06-22 10:49:09 -0300

    commit inicial

---
+++ app.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+table = pd.read_csv("telecom_users.csv")
+
+table = table.drop("Unnamed: 0", axis=1)
+print(table)
+
+
+table["TotalSpend"] = pd.to_numeric(table["TotalSpend"], errors="coerce")
+
+
+table = table.dropna(how="all", axis=1)
+
+table = table.dropna(how="any", axis=0)
+
+print(table.info())
+
+
+print(table["Churn"].value_counts())
+print(table["Churn"].value_counts(normalize=True).map("{:.1%}".format))
+
+
+import plotly.express as px
+
+
+for column in table.columns:
+    chart = px.histogram(table, x=column, color="Churn")
+    chart.show()
